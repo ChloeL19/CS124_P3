fb2046730acf497158f36c75b77ade18b2646a2a
Chloe Loughridge <cloughridge@college.harvard.edu>
2022-04-18 20:03:35 -0400

    retry with autograder3

--- hill_climbing.py
+++ hill_climbing.py
@@ -20,14 +20,16 @@ def hill_climb_n(inputfile, max_iter, verbose=False):
     if verbose:
         print(A)
     size = A.shape[0]
-    randsol = np.asarray([1 if random.random() < 0.5 else -1 for i in range(size)])
+    randsol = randsol2 = np.asarray([1 if random.random() < 0.5 else -1 for i in range(size)])
     for i in range(max_iter):
         tind = np.random.randint(0,size)
-        randsol2 = randsol
         randsol2[tind] = -1*randsol2[tind]
         if (compute_residue_n(A, randsol2.tolist()) < \
         compute_residue_n(A, randsol.tolist())):
             randsol = randsol2
+        else:
+            # chose a different random neighbor of orginal S
+            randsol2 = randsol
     return compute_residue_n(A, randsol), randsol
 
 def hill_climb_p(inputfile, max_iter):
